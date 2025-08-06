6bd884dfbd749f35c4b0ef98eb39e54838a6aca2
oleguss33 <oleguss0707@gmail.com>
2025-08-06 16:24:43 +0300

    изменено

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
-public class Main
-{
-    public static void main(String[] args)
-    {
+public class Main {
+    public static void main(String[] args) {
         BonusMilesService service = new BonusMilesService();
         int price = 10_000;
         int miles = service.calculate(price); // должно получиться 500
